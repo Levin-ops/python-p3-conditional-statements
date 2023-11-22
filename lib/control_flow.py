53c24f11fa1461ccceb9a64eed684344e185f1d8
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-22 22:19:28 +0300

    Admin function done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username == "admin" and password=="12345":
+        return("Access granted")
+    elif username =="ADMIN" and password == "12345":
+        return("Access granted")
+    elif username !="admin" and password !="12345":
+        return ("Access denied")
+    else:
+        return("Access denied")
+    
 
 def hows_the_weather(temperature):
     # your code here
@@ -15,3 +22,17 @@ def fizzbuzz(num):
 def calculator(operation, num1, num2):
     # your code here
     pass
+
+
+# dog = "hungry"
+
+# dict_map ={
+#     "hungry":"Refilling food bowl",
+#     "thirsty":"refilling water bowl",
+#     "playful":"Play tu-of-war",
+#     "cuddly":"Snuggling",
+# }
+
+# owner = dict_map.get(dog, "Reading newspaper")
+
+# print(owner)
